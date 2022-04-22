54d07ef23d4ce658e5e108aee39786d04f153d1f
r0808 <arthurdelophem3@gmail.com>
2022-04-22 17:42:03 +0200

    feat: add hardcoded get/create with mongoose schema

--- controllers/transfers.js
+++ controllers/transfers.js
@@ -1,21 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const TransferSchema = new Schema({
+    message: String,
+    from_user: String,
+    amount: String,
+    completed: Boolean
+})
+const Transfer = mongoose.model('Transfer', TransferSchema);
+
 const getAll = (req, res) => {
-    res.json({
-        "status": "success",
-        "data": {
-            "transfers": []
+    Transfer.find({}, (err, docs) => {
+        if (!err) {
+            res.json({
+                "status": "success",
+                "data": {
+                    "transfers": docs
+                }
+            })
         }
     })
+
+
 }
 
 const create = (req, res) => {
-    res.json({
-        "status": "success",
-        "data": {
-            "transfers": {
-                "text": "transaction 215"
-            }
-        }
+    let transfer = new Transfer();
+    transfer.message = "hey geef je mij mijn geld terug ajb dankjewel";
+    transfer.from_user = "uid_9918";
+    transfer.amount = "8";
+    transfer.completed = false;
+    transfer.save((err, doc) => {
+        if (!err) {
+            res.json({
+                "status": "success",
+                "data": {
+                    "transfers": {
+                        "transfer": doc
+                    }
+                }
+            })
+        };
+
     })
+
+
 }
 
 module.exports.getAll = getAll;
