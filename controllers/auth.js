2a44a92c8b36a2bf6e2e7aa316832b0dbc3f058e
r0808 <arthurdelophem3@gmail.com>
2022-04-23 16:36:10 +0200

    feat: add signup via postman to db

---
+++ controllers/auth.js
@@ -0,0 +1,19 @@
+const User = require('../models/User');
+
+const signup = async (req, res, next) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    const user = new User({ username: username });
+    await user.setPassword(password);
+    await user.save().then(result => {
+        res.json({
+            "status": "success",
+        })
+    }).catch(err => {
+        res.json({
+            "status": "failed",
+        })
+    })
+}
+
+module.exports.signup = signup;
