4695a0f9acbce248567802665a405bd141feba5d
r0808 <arthurdelophem3@gmail.com>
2022-05-07 22:30:18 +0200

    docs: remove passport from project

--- passport/passport.js
+++
@@ -1,8 +0,0 @@
-const passport = require('passport');
-const User = require('../models/user');
-const authController = require('../controllers/auth');
-
-// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
-passport.use(User.createStrategy({
-    usernameField: 'email', passReqToCallback: true
-}, authController.signup()));
