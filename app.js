c5f76b60f0ffd034446a786ad47b126e4c8bdd79
r0808 <arthurdelophem3@gmail.com>
2022-05-07 22:24:10 +0200

    feat: add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+var cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -14,6 +15,8 @@ mongoose.connect('mongodb://localhost:27017/imdCurrencyApp', {
 });
 
 const app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
